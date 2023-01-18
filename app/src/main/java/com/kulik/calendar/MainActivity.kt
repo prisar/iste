b458a2f17a61d01dafe8ae3a94860eb3465f2e9b
prisar <prisar230@gmail.com>
2023-01-18 12:18:08 +0530

    web

--- app/src/main/java/com/kulik/calendar/MainActivity.kt
+++ app/src/main/java/com/kulik/calendar/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.kulik.calendar
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.kulik.calendar.ui.theme.IsteTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CalenderDisplay()
                 }
             }
         }
@@ -30,14 +36,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CalenderDisplay() {
+    var backEnabled by remember { mutableStateOf(false) }
+    var webView: WebView? = null
+    val url = "https://agrohikulik.web.app/calender.html"
+
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            WebView(context).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = object : WebViewClient() {
+                    override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
+                        backEnabled = view.canGoBack()
+                    }
+                }
+                settings.javaScriptEnabled = true
+
+                loadUrl(url)
+                webView = this
+            }
+        }, update = {
+            webView = it
+        })
+
+    BackHandler(enabled = backEnabled) {
+        webView?.goBack()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     IsteTheme {
-        Greeting("Android")
+        CalenderDisplay()
     }
 }
